6bc37bb7f3e896d7d432638b96e0d68ac79ad9d0
kareem <kareemfathy312@gmail.com>
2020-12-21 03:30:08 +0200

    s
    s

--- app.js
+++ app.js
@@ -2,22 +2,28 @@ const express=require('express');
 const mongoose=require('mongoose');
 
 const AllStaffRoutes = require('./routes/StaffMembersRoutes');
+<<<<<<< HEAD
 <<<<<<< Updated upstream
 const AllHRRoutes = require('./routes/HrRoutes');
 =======
 const AllHrRoutes = require('./routes/hrRoutes');
 >>>>>>> Stashed changes
 
+=======
+>>>>>>> parent of e7733f6... s
 const app=express();
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use('/',AllStaffRoutes);
+<<<<<<< HEAD
 <<<<<<< Updated upstream
 app.use('/',AllHRRoutes);
 =======
 app.use('/hr',AllHrRoutes);
 >>>>>>> Stashed changes
 
+=======
+>>>>>>> parent of e7733f6... s
 app.listen(3000);
 
 module.exports=app;
