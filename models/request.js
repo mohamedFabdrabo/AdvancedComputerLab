8db9d07b2f6cb5caa0076ed793b453a9931786f6
ali lotfy <alilotfy100@gmail.com>
2020-12-22 11:02:38 +0200

    same as before

--- models/request.js
+++ models/request.js
@@ -2,9 +2,8 @@ const mongoose=require('mongoose');
 const Schema   = mongoose.Schema ;
 const RE=mongoose.Schema({
     id:{type:String,required:true,unique:true,sparse:true},
-
-    sender:{type:{type:Schema.Types.ObjectId ,ref:"location"},required:true},
-    receiver:[{type:{type:Schema.Types.ObjectId ,ref:"AM"},required:true}],
+    sender:{type:{type:Schema.Types.ObjectId ,ref:"location"}},
+    receiver:[{type:{type:Schema.Types.ObjectId ,ref:"AM"}}],
     state:{enum : ['Pending','Accepted','Rejected','Cancelled']},
     reason:String,
     type:{enum : ['Compensation','Replacement','Leave','Slot-linking','dayOffChange']}
