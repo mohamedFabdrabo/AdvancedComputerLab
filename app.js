db2359f7819375347b4cd31be1380b88c6c1864c
kareem <kareemfathy312@gmail.com>
2020-12-23 17:34:23 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,34 +2,15 @@ const express=require('express');
 const mongoose=require('mongoose');
 
 const AllStaffRoutes = require('./routes/StaffMembersRoutes');
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-const AllHRRoutes = require('./routes/HrRoutes');
-=======
-const AllHrRoutes = require('./routes/hrRoutes');
->>>>>>> Stashed changes
-
-=======
->>>>>>> parent of e7733f6... s
-=======
->>>>>>> parent of e7733f6... s
+//const HrRoutes = require('./routes/forMytest');
+//const HODRoutes = require('./routes/HODRoutes');
 const app=express();
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use('/',AllStaffRoutes);
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-app.use('/',AllHRRoutes);
-=======
-app.use('/hr',AllHrRoutes);
->>>>>>> Stashed changes
+//app.use('/',hrRoutes);
+//app.use('/',HODRoutes);
 
-=======
->>>>>>> parent of e7733f6... s
-=======
->>>>>>> parent of e7733f6... s
 app.listen(3000);
 
 module.exports=app;
