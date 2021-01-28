21c74fc63905265df139be4c29ee46cbf321da71
Ali Ali El-Dabaa <eldab3.2015@gmail.com>
2021-01-28 12:19:58 +0200

    a

--- app.js
+++ app.js
@@ -52,15 +52,6 @@ newC.save();
 
 //app.listen(3000);
 
-// Serve static assets if in production
-// if (process.env.NODE_ENV === 'production') {
-//     // Set static folder
-//     app.use(express.static('build'));
-  
-//     app.get('*', (req, res) => {
-//       res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-//     });
-//   }
 
 module.exports=app;
 
